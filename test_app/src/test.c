4389657095a13cfaa04913b35688cd50af58274d
Ime Prezime <ime.prezime@mail>
2022-05-22 15:02:56 +0200

    App receives device name via command line

--- test_app/src/test.c
+++ test_app/src/test.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
 #define MSG_LEN 50
 #define MAX_LEN (MSG_LEN + 49) // for #
 #define TEST_LEN 5
@@ -15,11 +16,12 @@
 #define SPACE_UNIT 1
 #define DEVICE_NAME "/dev/morse"
 #define NUM_OF_CHARACTERS 37
+#define MAX_DEVICE_NAME 255
 
 static char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
 
-char command_character = ' '; //this char is a shared resource
-char* random_array;
+char command_character = ' '; // this char is a shared resource
+char *random_array;
 int test_ind = -1;
 
 int file_desc;
@@ -28,16 +30,16 @@ int ret_val;
 pthread_t user_communication;
 pthread_t task;
 
-char* test_array[5] = {"MORSE", "TEST", "LINUX EMBEDDED", "2ROOM", "FIFTH ELEMENT"};
-char* codes[5] = {"--#---#.-.#...#.", "-#.#...#-",".-..#..#-.#..-#-..-# #.#--#-...#.#-..#-..#.#-..", "..---#.-.#---#---#--","..-.#..#..-.#-#....# #.#.-..#.#--#.#-.#-"};
-char* output_array;
+char *test_array[5] = {"MORSE", "TEST", "LINUX EMBEDDED", "2ROOM", "FIFTH ELEMENT"};
+char *codes[5] = {"--#---#.-.#...#.", "-#.#...#-", ".-..#..#-.#..-#-..-# #.#--#-...#.#-..#-..#.#-..", "..---#.-.#---#---#--", "..-.#..#..-.#-#....# #.#.-..#.#--#.#-.#-"};
+char *output_array;
 sem_t shared;
 sem_t synchro;
 
-void* user_comm_foo(void* p);
-void* task_foo(void* p);
+void *user_comm_foo(void *p);
+void *task_foo(void *p);
 
-void* user_comm_foo(void* p)
+void *user_comm_foo(void *p)
 {
     /*commented block that follows is only needed for additional task
      *    char diode_number;
@@ -50,111 +52,118 @@ void* user_comm_foo(void* p)
      *  scanf("%d", &time_unit);
      */
 
-    //how to be sure that user inserted one char exactly? Not neccessary but still curious
-    while(1)
+    // how to be sure that user inserted one char exactly? Not neccessary but still curious
+    while (1)
     {
-        while (command_character != 'N' && command_character != 'Q' && command_character != 'T'
-        && command_character != 'n' && command_character != 'q' && command_character != 't')
+        while (command_character != 'N' && command_character != 'Q' && command_character != 'T' && command_character != 'n' && command_character != 'q' && command_character != 't')
         {
-            if(command_character != '\r' && command_character != '\n')
+            if (command_character != '\r' && command_character != '\n')
             {
                 printf("\n====================\nEnter work regime\nN/n - normal; T/t - test; !/q - quit\n");
             }
             scanf("%c", &command_character);
-            if(command_character == 't' || command_character == 'T')
+            if (command_character == 't' || command_character == 'T')
             {
 
                 do
                 {
                     printf("Enter test array index, 0-4\n");
                     scanf("%d", &test_ind);
-                }while (test_ind < 0 && test_ind > 4);
+                } while (test_ind < 0 && test_ind > 4);
             }
         }
         sem_post(&shared);
 
-        if(command_character == 'q' || command_character == 'Q')
+        if (command_character == 'q' || command_character == 'Q')
         {
             break;
         }
         else
         {
-            //waits until task finishes and requests next iteration instead of exiting; maybe
-        //way I imagined it isn't quite right
+            // waits until task finishes and requests next iteration instead of exiting; maybe
+            // way I imagined it isn't quite right
             sem_wait(&synchro);
         }
     }
 }
 
-void* task_foo(void* p)
+void *task_foo(void *p)
 {
     int i;
-    while(1)
+    const char *dname = (char *)p;
+
+    while (1)
     {
         sem_wait(&shared);
-        
+
         if (command_character == 'n' || command_character == 'N')
         {
             printf("Normal regime starts!\n");
-            
 
-	    printf(DEVICE_NAME" device is successfully opened! %d\n", file_desc);
-            
-            while(1){
-                    file_desc = open(DEVICE_NAME, O_RDWR);
+            printf("%s device is successfully opened! %d\n", dname, file_desc);
 
-	    	    if(file_desc < 0)
-	    	    {
-	 	    	printf(DEVICE_NAME " device isn't open\n");
-			printf("Try:\t1) Check does "DEVICE_NAME" node exist\n\t2)'chmod 666 "DEVICE_NAME"\n\t3) ""insmod"" morse module\n");
-			sem_post(&synchro);
-			exit(-1);
-	    	    }
-	    
-            	    int len = 0;
-            	    len = rand() % MSG_LEN;
-            	    printf("%d - lenght of the message\n", len);
-		    random_array = malloc(MSG_LEN * sizeof(char));
-		    if(random_array){
-		    	int key;
-		    	for(i = 0; i < len; i++){
-		    		key = rand() % NUM_OF_CHARACTERS;
-		    		random_array[i] = charset[key];
-		    	}
-		    	random_array[len] = '\0';
-		    	printf("MESSAGE: %s\n", random_array);
-		    
-		    	ret_val = write(file_desc, random_array,len);
-	    	    	printf("Written message: %s\n", random_array);
-	            	printf("write ret_val: %d\n", ret_val);
-	            	
-	            	/* Reopen /dev/morse device. */
-	    	    	close(file_desc);
-	    	    	file_desc = open(DEVICE_NAME, O_RDWR);
+            while (1)
+            {
+                file_desc = open(dname, O_RDWR);
 
-	    	    	if(file_desc < 0)
-	    	    	{
-		    		printf(DEVICE_NAME " device isn't open\n");
-				printf("Try:\t1) Check does "DEVICE_NAME" node exist\n\t2)'chmod 666 "DEVICE_NAME"\n\t3) ""insmod"" morse module\n");
-				sem_post(&synchro);
-				exit(-1);
-	    	    	}
-            
-	            	/* Init temp buffer. */
-	    	    	memset(output_array, 0, MAX_LEN);
+                if (file_desc < 0)
+                {
+                    printf("%s device isn't open or doesn't exist: exiting application!\n", dname);
+                    // printf("Try:\t1) Check does " DEVICE_NAME " node exist\n\t2)'chmod 666 " DEVICE_NAME "\n\t3) "
+                    //        "insmod"
+                    //        " morse module\n");
+                    sem_post(&synchro);
+                    exit(-1);
+                }
 
-	    	    	/* Read from /dev/morse device. */
-	    	    	printf("Reading...\n");
-	            	ret_val = read(file_desc, output_array, MAX_LEN);
-	            	printf("Driver has returned message: %s\n", output_array);
-	            	printf("ret_val: %d\n", ret_val);
+                int len = 0;
+                len = rand() % MSG_LEN;
+                printf("%d - lenght of the message\n", len);
+                random_array = malloc(MSG_LEN * sizeof(char));
+                if (random_array)
+                {
+                    int key;
+                    for (i = 0; i < len; i++)
+                    {
+                        key = rand() % NUM_OF_CHARACTERS;
+                        random_array[i] = charset[key];
+                    }
+                    random_array[len] = '\0';
+                    printf("MESSAGE: %s\n", random_array);
 
-	            	/* Close /dev/morse device. */
-	            	close(file_desc);
-		    }
-		    free(random_array);
-		    sleep(2);
-	    }
+                    ret_val = write(file_desc, random_array, len);
+                    printf("Written message: %s\n", random_array);
+                    printf("write ret_val: %d\n", ret_val);
+
+                    /* Reopen /dev/morse device. */
+                    close(file_desc);
+                    file_desc = open(dname, O_RDWR);
+
+                    if (file_desc < 0)
+                    {
+                        printf("%s device isn't open or doesn't exist: exiting application!\n", dname);
+                        // printf("Try:\t1) Check does " DEVICE_NAME " node exist\n\t2)'chmod 666 " DEVICE_NAME "\n\t3) "
+                        //        "insmod"
+                        //        " morse module\n");
+                        sem_post(&synchro);
+                        exit(-1);
+                    }
+
+                    /* Init temp buffer. */
+                    memset(output_array, 0, MAX_LEN);
+
+                    /* Read from /dev/morse device. */
+                    printf("Reading...\n");
+                    ret_val = read(file_desc, output_array, MAX_LEN);
+                    printf("Driver has returned message: %s\n", output_array);
+                    printf("ret_val: %d\n", ret_val);
+
+                    /* Close /dev/morse device. */
+                    close(file_desc);
+                }
+                free(random_array);
+                sleep(2);
+            }
             command_character = ' ';
             /*actual job*/
             sem_post(&synchro);
@@ -162,57 +171,62 @@ void* task_foo(void* p)
         else if (command_character == 't' || command_character == 'T')
         {
             printf("Test regime starts!\n");
-            file_desc = open(DEVICE_NAME, O_RDWR);
+            file_desc = open(dname, O_RDWR);
 
-	    if(file_desc < 0)
-	    {
-		printf(DEVICE_NAME " device isn't open\n");
-		printf("Try:\t1) Check does "DEVICE_NAME" node exist\n\t2)'chmod 666 "DEVICE_NAME"\n\t3) ""insmod"" morse module\n");
-		sem_post(&synchro);
-		exit(-1);
-	    }
-	    
-	    printf(DEVICE_NAME" device is successfully opened! %d\n", file_desc);
+            if (file_desc < 0)
+            {
+                printf("%s device isn't open or doesn't exist: exiting application!\n", dname);
+                // printf("Try:\t1) Check does " DEVICE_NAME " node exist\n\t2)'chmod 666 " DEVICE_NAME "\n\t3) "
+                //        "insmod"
+                //        " morse module\n");
+                sem_post(&synchro);
+                exit(-1);
+            }
 
-	    /* Write to /dev/morse device. */
-	    ret_val = write(file_desc, test_array[test_ind], strlen(test_array[test_ind]));
-	    printf("Written message: %s\n", test_array[test_ind]);
-	    printf("write ret_val: %d\n", ret_val);
+            printf("%s device is successfully opened! %d\n", dname,  file_desc);
 
-	    /* Reopen /dev/morse device. */
-	    close(file_desc);
-	    file_desc = open(DEVICE_NAME, O_RDWR);
+            /* Write to /dev/morse device. */
+            ret_val = write(file_desc, test_array[test_ind], strlen(test_array[test_ind]));
+            printf("Written message: %s\n", test_array[test_ind]);
+            printf("write ret_val: %d\n", ret_val);
 
-	    if(file_desc < 0)
-	    {
-		printf(DEVICE_NAME " device isn't open\n");
-		printf("Try:\t1) Check does "DEVICE_NAME" node exist\n\t2)'chmod 666 "DEVICE_NAME"\n\t3) ""insmod"" morse module\n");
-		sem_post(&synchro);
-		exit(-1);
-	    }
-            
-	   /* Init temp buffer. */
-	    memset(output_array, 0, MAX_LEN);
+            /* Reopen /dev/morse device. */
+            close(file_desc);
+            file_desc = open(dname, O_RDWR);
 
-	    /* Read from /dev/morse device. */
-	    printf("Reading...\n");
-	    ret_val = read(file_desc, output_array, MAX_LEN);
-	    printf("Driver has returned message: %s\n", output_array);
-	    printf("ret_val: %d\n", ret_val);
+            if (file_desc < 0)
+            {
+                printf("%s device isn't open or doesn't exist: exiting application!\n", dname);
+                // printf("Try:\t1) Check does " DEVICE_NAME " node exist\n\t2)'chmod 666 " DEVICE_NAME "\n\t3) "
+                //        "insmod"
+                //        " morse module\n");
+                sem_post(&synchro);
+                exit(-1);
+            }
 
-	    /* Close /dev/morse device. */
-	    close(file_desc);
-	    
-	    if(!strncmp(output_array, codes[test_ind], strlen(codes[test_ind]))){
-	    	printf("Test successful. Morse code validated.\n");
-	    }
-	    else
-	    {
-	    	printf("Test number %d failed.\n", test_ind);
-	    	//sem_post(&synchro);
-	    	//exit(-1);
-	    }
-            
+            /* Init temp buffer. */
+            memset(output_array, 0, MAX_LEN);
+
+            /* Read from /dev/morse device. */
+            printf("Reading...\n");
+            ret_val = read(file_desc, output_array, MAX_LEN);
+            printf("Driver has returned message: %s\n", output_array);
+            printf("ret_val: %d\n", ret_val);
+
+            /* Close /dev/morse device. */
+            close(file_desc);
+
+            if (!strncmp(output_array, codes[test_ind], strlen(codes[test_ind])))
+            {
+                printf("Test successful. Morse code validated.\n");
+            }
+            else
+            {
+                printf("Test number %d failed.\n", test_ind);
+                // sem_post(&synchro);
+                // exit(-1);
+            }
+
             command_character = ' ';
 
             sem_post(&synchro);
@@ -223,27 +237,38 @@ void* task_foo(void* p)
             break;
         }
     }
-    
 }
 
-
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
-    srand(time(NULL));
-    //random_array = malloc(MAX_LEN * sizeof(char));
+
+    if (argc != 2)
+    {
+        printf("Invalid number of argumens detected! Exiting.\n");
+        exit(-1);
+    }
+
+    char *dev_name = malloc(MAX_DEVICE_NAME * sizeof(char));
     output_array = malloc(MAX_LEN * sizeof(char));
-    /*task will block on shared if user_comm hasn't written anything into command_char, after 
+
+    srand(time(NULL));
+    strncpy(dev_name, argv[1], strlen(argv[1]));
+
+    /*task will block on shared if user_comm hasn't written anything into command_char, after
     user_comm writes it signals semaphore */
     sem_init(&shared, 0, 0);
     sem_init(&synchro, 0, 0);
 
-    pthread_create(&user_communication, NULL, user_comm_foo, NULL);
-    pthread_create(&task, NULL, task_foo, NULL);
+    pthread_create(&user_communication, NULL, user_comm_foo, (void *)dev_name);
+    pthread_create(&task, NULL, task_foo, (void *)dev_name);
 
     pthread_join(user_communication, NULL);
     pthread_join(task, NULL);
 
     sem_destroy(&synchro);
     sem_destroy(&shared);
+
+    free(dev_name);
+
     return 0;
 }
